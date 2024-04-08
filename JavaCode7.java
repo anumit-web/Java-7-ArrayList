059a8cb7a3a67d79eb0b07e8b3498e0200a9fd0a
Anumit Jooloor <anumit@gmail.com>
2024-04-08 15:35:14 -0700

    Add files

---
+++ JavaCode7.java
@@ -0,0 +1,27 @@
+/*
+/usr/local/Cellar/openjdk/21.0.2/bin/javac JavaCode7.java
+/usr/local/Cellar/openjdk/21.0.2/bin/java JavaCode7
+*/
+
+// import
+import java.util.*;
+
+public class JavaCode7 {
+
+    public static void main(String[] args) {
+        System.out.println("------------------------------------------------------------");
+
+        System.out.println("Java Code 7");
+        System.out.println("Java - ArrayList");
+        System.out.println("");
+
+        // Display Current Date            
+        ArrayList<String>  the_Lord_of_the_Rings_The_Rings_of_Power_main_characters = new ArrayList<String>();
+        the_Lord_of_the_Rings_The_Rings_of_Power_main_characters.add("Galadriel");
+        the_Lord_of_the_Rings_The_Rings_of_Power_main_characters.add("Gil-galad");
+        the_Lord_of_the_Rings_The_Rings_of_Power_main_characters.add("Eärien");
+        the_Lord_of_the_Rings_The_Rings_of_Power_main_characters.add("Celebrimbor");
+        System.out.println(the_Lord_of_the_Rings_The_Rings_of_Power_main_characters);
+        
+    }
+}
